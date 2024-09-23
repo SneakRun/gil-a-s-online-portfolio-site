3bc32954b46637e47b468ee8f70292b1920e7307
Gil <gil.a.sherman@gmail.com>
2024-09-23 20:29:40 +0100

    capitalised-descriptions-correctly

--- javascript/portfolio-grid.js
+++ javascript/portfolio-grid.js
@@ -3,67 +3,67 @@ const portfolioItems = [
   {
     id: 'titus-lempereur',
     title: 'Titus L\'Empereur: Backdrop Projection Design',
-    description: 'Designed In Blender for the Carl Maria von Webern Theatre in Munich, Germany, 2024',
+    description: 'Designed in Blender for the Carl Maria von Webern Theatre in Munich, Germany, 2024',
     image: 'videos/titus.webp'
   },
   {
     id: 'john-hardy',
     title: 'R/GA Contract: John Hardy \'Moment Maker\'',
-    description: 'Web-Based Procedural Flower Interface for User-End Shopping Experience, 2024',
+    description: 'Web-based procedural flower interface for user-end shopping experience, 2024',
     image: 'videos/john-hardy.webp'
   },
   {
     id: 'gasoline-valley',
     title: 'It Took Three Weeks To Scroll Through The Gasoline Valley',
-    description: 'Digital World Made for Central Saint Martins Virtual Group Show, made in Blender, 2022',
+    description: 'Digital world made for Central Saint Martins Virtual Group Show, made in Blender, 2022',
     image: 'videos/gasoline-valley.webp'
   },
   {
     id: 'zeta-angel',
     title: 'Zeta_Angel',
-    description: 'Unity-Based Interactive Game Environment, made for Hypha Studios\' \'HARD WIRED\' Exhibition, London, 2024',
+    description: 'Unity-based interactive game environment, made for Hypha Studios\' \'HARD WIRED\' Exhibition, London, 2024',
     image: 'videos/zeta-angel-cover.webp'
   },
   {
     id: 'neo-starcadia',
     title: 'Neo-Starcadia',
-    description: 'Unity-Based Interactive Illustration Experience, made for Creative Computing Institute Grad Show, 2022',
+    description: 'Unity-based interactive illustration experience, made for Creative Computing Institute Grad Show, 2022',
     image: 'videos/neo-starcadia.webp'
   },
   {
     id: 'dream-eater',
     title: 'Dream Eater',
-    description: 'Unity-Based Interactive Game Environment, made for Central Saint Martins Degree Show, 2023',
+    description: 'Unity-based interactive game environment, made for Central Saint Martins Degree Show, 2023',
     image: 'videos/dream-eater.webp'
   },
   {
     id: 'online-portfolio',
     title: 'Online Portfolio Website',
-    description: 'Made in HTML, CSS, and Javascript, Rebuilt Completely for 2024',
+    description: 'Made in HTML, CSS, and Javascript, rebuilt completely for 2024',
     image: 'videos/online-portfolio.webp'
   },
   {
     id: 'pulse-out',
     title: 'Pulse-Out!',
-    description: 'AI-Assisted Web-Based Arcade Game, made in Javascript, 2024',
+    description: 'AI-assisted web-based arcade game, made in Javascript, 2024',
     image: 'videos/pulse-out.webp'
   },
   {
     id: 'cubitts',
     title: 'Sunglasses Design Commission for Cubitts',
-    description: 'Designed and Manufactured as Part of Cubitts\' \'Artists of Hampstead\' Collection, 2022',
+    description: 'Designed and manufactured as part of Cubitts\' \'Artists of Hampstead\' Collection, 2022',
     image: 'images/cubitts.webp'
   },
   {
     id: 'plant-growth-visualiser',
     title: 'Plant Growth Visualiser',
-    description: 'Arduino x p5.js Product Prototype for Human-Centred Digital Design, 2022',
+    description: 'Arduino x p5.js product prototype for human-centred digital design, 2022',
     image: 'images/plant-growth-vis.webp'
   },
   {
     id: 'remesh',
     title: 'Remesh',
-    description: 'Procedurally Generated Typeface, made in Blender, 2023',
+    description: 'Procedurally generated typeface, made in Blender, 2023',
     image: 'videos/remesh.webp'
   }
 ];
